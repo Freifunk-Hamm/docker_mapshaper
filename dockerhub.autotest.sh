bb69ef597873fb8541241e13c34acd198b5aefa0
Christian Weiss <christian.weiss@web-impressions.de>
2019-11-02 11:34:32 +0100

    try to launch another container from within a docker container

--- dockerhub.autotest.sh
+++ dockerhub.autotest.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+echo "Execute another docker container from within this test environment container:"
+docker pull hello-world
+docker run --rm hello-world
 echo "Tests successful."
 exit 0
