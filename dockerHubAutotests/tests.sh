74b8c434db312c7ef2711a45bc8e6d07a7ab4926
Christian Weiss <christian.weiss@web-impressions.de>
2019-11-11 21:42:43 +0100

    tests.sh path fixed. Wait for report with a 10 minutes timeout.

--- dockerHubAutotests/tests.sh
+++ dockerHubAutotests/tests.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 echo "Available environment variables:"
 env
+
 echo "Wait for Trivy scan result..."
-while [ ! -f /projectroot/report.json ]; do sleep 1; done
+timeout=0
+while [ ! -f /projectroot/report.json ]; do
+    sleep 1
+    timeout=$((timeout + 1))
+    if [ $timeout -ge 600 ]; then
+        echo "Exit due to a timeout. We do not want to wait longer then 10 minutes."
+        exit
+    fi
+done
+
 echo "Send report to Trivy Report Registry..."
 curl -X POST -H "Content-Type: application/json" -d @/projectroot/report.json https://trivy-report-registry.ff-hamm.de/incomming-report
 
