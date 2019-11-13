3ee774e430836858487f74b9fa4e0c48c2630392
Christian Weiss <christian.weiss@web-impressions.de>
2019-11-13 16:02:39 +0100

    run trivy direct as SUT, trivy image extended with curl, fails if notable vulns are detected, prevent report.json from being committed

--- dockerHubAutotests/tests.sh
+++ dockerHubAutotests/tests.sh
@@ -2,24 +2,30 @@
 echo "Available environment variables:"
 env
 
-echo "Wait for Trivy scan result..."
-timeout=0
-while [ ! -f /projectroot/report.json ]; do
-    sleep 1
-    timeout=$((timeout + 1))
-    if [ $timeout -ge 1800 ]; then
-        echo "Exit due to a timeout. We do not want to wait longer then 30 minutes."
-        exit 1
-    fi
-done
+echo "Trivy version"
+trivy -v
+
+echo "Generate vulnerability report..."
+trivy --no-progress --format json --output /projectroot/report.json "${DOCKER_REPO}:${DOCKER_TAG}"
+echo "Show vulnerabilities summary: "
+trivy --quiet "${DOCKER_REPO}:${DOCKER_TAG}"
+
 echo "Current directory contents:"
 ls -al /projectroot
 
 echo "Content of Trivy Report"
 cat /projectroot/report.json
+echo ""
 
 echo "Send report to Trivy Report Registry..."
 curl -X POST -H "Content-Type: application/json" --silent --data-binary "@/projectroot/report.json" https://trivy-report-registry.ff-hamm.de/incoming-report
+echo "done."
+
+# fails if vulnerabilities of a notable severity are detected:
+if trivy --quiet --severity CRITICAL,HIGH --exit-code 1 "${DOCKER_REPO}:${DOCKER_TAG}" > /dev/null 2>&1; then
+  echo "Tests failed! Notable vulnerabilities are detected!"
+  exit 1
+fi
 
 echo "Tests successful."
 exit 0
