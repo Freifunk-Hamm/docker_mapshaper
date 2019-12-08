11269c786f7e4a6b46cb716ca3343d68dc3fd374
Christian Weiss <christian.weiss@web-impressions.de>
2019-12-08 07:02:38 +0100

    Trivy: scan local image, not previously uploaded image

--- dockerHubAutotests/tests.sh
+++ dockerHubAutotests/tests.sh
@@ -8,10 +8,24 @@ env
 echo "Trivy version"
 trivy -v
 
+echo "Docker images:"
+docker image list
+echo "Export docker image as: this.tar"
+docker save this:latest -o this.tar
+ls -al this.tar
+
+echo "Clean-up trivy cache (to get rid of old artifacts from previous runs):"
+trivy --clear-cache
+
+DOCKER_REPO_NO_REG=$(echo $IMAGE_NAME | awk -F"/" '{print $2 "/" $3}')
+echo "Temp. docker image name is: ${DOCKER_REPO_NO_REG}"
+echo "Temp. docker image name is: this:latest"
+echo "Official docker image name is: ${DOCKER_REPO}:${DOCKER_TAG}"
+
 echo "Generate vulnerability report..."
-trivy --no-progress --format json --output /projectroot/report.json "${DOCKER_REPO}:${DOCKER_TAG}"
+trivy -d --no-progress --format json --output /projectroot/report.json  --input this.tar
 echo "Show vulnerabilities summary: "
-trivy --quiet "${DOCKER_REPO}:${DOCKER_TAG}"
+trivy -d --quiet  --input this.tar
 
 echo "Current directory contents:"
 ls -al /projectroot
@@ -25,7 +39,7 @@ curl -X POST -H "Content-Type: application/json" --silent --data-binary "@/proje
 echo "done."
 
 # fails if vulnerabilities of a notable severity are detected:
-trivy --quiet --severity CRITICAL,HIGH --exit-code 1 "${DOCKER_REPO}:${DOCKER_TAG}" > /dev/null 2>&1
+trivy -d --quiet --severity CRITICAL,HIGH --exit-code 1  --input this.tar > /dev/null 2>&1
 exitCode=$?
 if [ $exitCode -gt 0 ]; then
   echo "Tests failed! Notable vulnerabilities are detected!"
